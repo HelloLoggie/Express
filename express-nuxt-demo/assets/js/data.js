3559602b8635c788e0cc959c78278b929a15780a
gelq <gelq@Ctrip.com>
2017-08-18 18:31:25 +0800

    数据修改

--- express-nuxt-demo/assets/js/data.js
+++ express-nuxt-demo/assets/js/data.js
@@ -80,3 +80,80 @@ export function getUrl(){
     }
     path = "restapi/soa2/12600/Flight/FlightXProductDetailInfo";
 }
+
+/*function getQuery(paramName) {
+ var path = /\?([^#]*)(#|$)/.exec(location.href);
+ if (path) {
+ path = new RegExp("(^|&)" + encodeURIComponent(paramName).replace(/([\-.*+?^${}()|[\]\/\\])/g, '\\$1') + "=([^&]*)(&|$)", "i").exec(path[1]);
+ if (path) return decodeURIComponent(path[2]);
+ }
+ return null;
+ }
+
+ function getUrl() {
+ var domain = '';
+ //  var protocol = location.protocol;
+ var protocol = "http:";
+
+
+ // fat http
+ // http://gateway.m.fws.qa.nt.ctripcorp.com/restapi/soa2/10771/Flight/FlightXProductDetailInfo
+ // https://gateway.m.fws.qa.nt.ctripcorp.com/restapi/soa2/10771/Flight/FlightXProductDetailInfo
+ //
+ // prd http
+ // http://m.ctrip.com/restapi/soa2/10771/Flight/FlightXProductDetailInfo
+ // https://sec-m.ctrip.com/restapi/soa2/10771/Flight/FlightXProductDetailInfo
+
+
+ if (env == 'prod') { // 生产
+ domain = protocol === 'https' ? 'sec-m.ctrip.com' : 'm.ctrip.com';
+ protocol = "https:";
+ } else if(env == 'fws'){ // 其他
+ domain = 'gateway.m.fws.qa.nt.ctripcorp.com';
+ }
+ path = "restapi/soa2/12600/Flight/FlightXProductDetailInfo";
+
+ return protocol + "//" + domain + "/" + path;
+ }
+
+ function getData(path, data, callback) {
+ var XmlHttp = new XMLHttpRequest();
+ XmlHttp.open("POST", path, true);
+ XmlHttp.setRequestHeader("Content-Type", "application/json");
+ XmlHttp.onreadystatechange = function () {
+ if (XmlHttp.readyState == 4) {
+ var Result = false;
+ if (XmlHttp.status == 200) {
+ Result = XmlHttp.responseText;
+ }else{
+ console.log( XmlHttp.status );
+ }
+ XmlHttp = null;
+
+ callback && callback(Result);
+ }
+ };
+
+ XmlHttp.send(JSON.stringify(data));
+ }
+
+ var env = getQuery('env');
+ var qInfo = getQuery('queryInfo');
+ var param = {
+ "head": {
+ "cid": "12001132610000020998",
+ // "cid": "",
+ "ctok": "",
+ "cver": "611",
+ "lang": "01",
+ "sid": "8890",
+ "syscode": "12",
+ // "auth": ""
+ "auth": "FD47C3DFCB4354848C568F815A9CE2C23B84228C878E72E99586AEE65CAF86F0"
+ },
+ "ver": 0,
+ "stype": 0,
+ "qInfo": qInfo
+ };
+ // 判断环境
+ var apiPath = getUrl();*/
