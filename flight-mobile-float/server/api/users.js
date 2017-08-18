0f62d89abb8c3045339cb4d040f2deadcb693f1c
gelq <gelq@Ctrip.com>
2017-08-18 18:34:59 +0800

    flight-mobile-float

---
+++ flight-mobile-float/server/api/users.js
@@ -0,0 +1,28 @@
+import { Router } from 'express'
+
+// 创建可安装的模块化路由处理程序，Router（）是完整的中间件和路由系统
+const router = Router()
+
+// Mock Users
+const users = [
+  { name: 'Alexandre' },
+  { name: 'Pooya' },
+  { name: 'Sébastien' }
+]
+
+/* GET users listing. */
+router.get('/users', function (req, res, next) {
+  res.json(users)
+})
+
+/* GET user by ID. */
+router.get('/users/:id', function (req, res, next) {
+  const id = parseInt(req.params.id)
+  if (id >= 0 && id < users.length) {
+    res.json(users[id])
+  } else {
+    res.sendStatus(404)
+  }
+})
+
+export default router
